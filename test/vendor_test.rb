ee1aa030492bef72aacf750b746231d55cab73f2
Tyla Devon <tyladevon@gmail.com>
2019-09-11 13:35:10 -0600

    Correct number of arugments being passed for check_stock test. Add stock and test that stock and check_stock are working properly

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -21,7 +21,11 @@ class VendorTest < Minitest::Test
   def test_check_stock
     assert_equal 0, @vendor.check_stock("Peaches")
     @vendor.stock("Peaches", 30)
-    assert_equal 30, @vendor.check_stock("Peaches", 30)
+    assert_equal 30, @vendor.check_stock("Peaches")
+    @vendor.stock("Peaches", 25)
+    assert_equal 55, @vendor.check_stock("Peaches")
+    @vendor.stock("Tomatoes", 12)
+    assert_equal ({"Peaches"=>55, "Tomatoes"=>12}), @vendor.inventory
   end
 
 
