1e2231371e56053477373a0a4ed0b9b0c1380e18
Tyla Devon <tyladevon@gmail.com>
2019-09-11 13:27:42 -0600

    Add check_stock and stock methods to Vendor class and att inventory to attr_reader

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,7 +1,17 @@
 class Vendor
-  attr_reader :name
+  attr_reader :name, :inventory
 
   def initialize(name)
     @name = name
+    @inventory = Hash.new(0)
   end
+
+  def check_stock(produce)
+    @inventory[produce]
+  end
+
+  def stock(produce, quantity)
+    @inventory[produce] += quantity
+  end
+  
 end
