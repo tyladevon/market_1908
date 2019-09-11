64dced6672fff9f4673e7ff87ea102d612c306e6
Tyla Devon <tyladevon@gmail.com>
2019-09-11 13:38:10 -0600

    Add class Market Test, assert an instance of market exists

---
+++ test/market_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+require './lib/market'
+require 'pry'
+
+class MarketTest < Minitest::Test
+
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
+end
