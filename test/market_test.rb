6d959a118943e652291c8efef31df222690a1747
Tyla Devon <tyladevon@gmail.com>
2019-09-11 14:57:58 -0600

    Add test for sorted_items_list

--- test/market_test.rb
+++ test/market_test.rb
@@ -56,4 +56,18 @@ class MarketTest < Minitest::Test
     assert_equal [@vendor_2], @market.vendors_that_sell("Banana Nice Cream")
   end
 
+  def test_sorted_items_list
+    @vendor_1.stock("Peaches", 35)
+    @vendor_1.stock("Tomatoes", 7)
+    @vendor_2.stock("Banana Nice Cream", 50)
+    @vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    @vendor_3.stock("Peaches", 65)
+
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+    
+    assert_equal ["Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peaches", "Tomatoes"], @market.sorted_item_list
+  end
+
 end
