3d1022ecbe034c53542af1e147d39464f89783dc
Tyla Devon <tyladevon@gmail.com>
2019-09-11 13:55:55 -0600

    Add vendors to attr_reader and initialize method. Also add add_vendors to Market class.

--- lib/market.rb
+++ lib/market.rb
@@ -1,8 +1,14 @@
+
 class Market
-  attr_reader :name
+  attr_reader :name, :vendors
 
   def initialize(name)
     @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
   end
 
 end
