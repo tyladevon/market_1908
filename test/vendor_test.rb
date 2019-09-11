7bb0dbee43592a56f958ec8218cdbf705f6ba335
Tyla Devon <tyladevon@gmail.com>
2019-09-11 13:10:57 -0600

    Create vendor_test and write test for existence and initialize

---
+++ test/vendor_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+require 'pry'
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_initialize
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+  end
+
+end
